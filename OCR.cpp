773a21afe81e13518cc599bf0b94da42436c6ceb
Corey Ford <coyotebush22@gmail.com>
2009-04-10 18:15:42 +0000

    basic structure

---
+++ OCR.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file OCR.cpp
+ * @author Corey Ford <fordco@sonoma.edu>
+ * @date Spring 2009
+ * @brief The implementation of the OCR class.
+ * @see OCR.h for the interface and documentation.
+ */
+/*
+ * Copyright 2009 Corey Ford
+ *
+ * This file is part of OCR.
+ *
+ * OCR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OCR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCR.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "OCR.h"
+
+OCR::OCR()
+{
+}
+
+OCR::~OCR()
+{
+}
---
+++ OCR.h
@@ -0,0 +1,38 @@
+/**
+ * @file OCR.h
+ * @author Corey Ford <fordco@sonoma.edu>
+ * @date Spring 2009
+ * @brief The interface and documentation of the OCR class.
+ * @see OCR.h for the implementation.
+ */
+/*
+ * Copyright 2009 Corey Ford
+ *
+ * This file is part of OCR.
+ *
+ * OCR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OCR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCR.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OCR_H_
+#define OCR_H_
+#include "EasyBMP/EasyBMP.h"
+
+class OCR
+{
+public:
+	OCR();
+	virtual ~OCR();
+};
+
+#endif /*OCR_H_*/
