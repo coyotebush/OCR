b9070e001d452c895b18bf4107cfeb5b1ac84b8f
Corey Ford <coyotebush22@gmail.com>
2009-04-11 17:12:06 +0000

    Omophagic Content Reader

--- TextLine.h
+++ TextLine.h
@@ -8,26 +8,26 @@
 /*
  * Copyright 2009 Corey Ford
  *
- * This file is part of GraphemeResolver.
+ * This file is part of Omophagic Content Reader.
  *
- * GraphemeResolver is free software: you can redistribute it and/or modify
+ * Omophagic Content Reader is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * GraphemeResolver is distributed in the hope that it will be useful,
+ * Omophagic Content Reader is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GraphemeResolver.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Omophagic Content Reader.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef TEXTLINE_H_
 #define TEXTLINE_H_
 
-namespace GraphemeResolver
+namespace OCR
 {
 /**
  * A line of text in the image.
--- main.cpp
+++ main.cpp
@@ -4,9 +4,27 @@
  * @author corey
  * @brief An application program using the GraphemeResolver library.
  */
+/*
+ * Copyright 2009 Corey Ford
+ *
+ * This file is part of Omophagic Content Reader.
+ *
+ * Omophagic Content Reader is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Omophagic Content Reader is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Omophagic Content Reader.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "Reader.h"
-using namespace GraphemeResolver;
+using namespace OCR;
 
 int main ()
 {
