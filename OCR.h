018aa45dc5ee0b31a91312bbcb15c80b8934b8c0
Corey Ford <coyotebush22@gmail.com>
2009-04-24 15:11:57 +0000

    comment tweak and svn:ignore changes

--- OCR.h
+++ OCR.h
@@ -149,7 +149,7 @@ bool isSimilar(RGBApixel * a, RGBApixel * b);
  * @param[in]     image     the bitmap image
  * @param[in]     start     starting point
  * @param[in,out] visited   which pixels have been visited.
- *                          Should be the same size as the area of limit.
+ *                          Should have the same dimensions as limit.
  * @param[in]     limit     do not search beyond this box
  * @return                  extent of contiguous pixels found
  */
