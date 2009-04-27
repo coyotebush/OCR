55d95f037f10a90a6859a104647a06448e2324d3
Corey Ford <coyotebush22@gmail.com>
2009-04-27 04:59:09 +0000

    copyright notices

--- src/Font.h
+++ src/Font.h
@@ -6,6 +6,25 @@
  * @see Font.cpp for the implementation.
  * @version $Id$
  */
+/*
+ * Copyright 2009 Corey Ford
+ *
+ * This file is part of Omophagic Content Reader.
+ *
+ * Omophagic Content Reader is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Omophagic Content Reader is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Omophagic Content Reader.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef OCR_FONT_H_
 #define OCR_FONT_H_
