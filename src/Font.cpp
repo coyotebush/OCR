8433da46ecfdfa3d2b25ea0e693d073f9ca70e3b
Corey Ford <coyotebush22@gmail.com>
2009-05-07 05:52:54 +0000

    Give quadrant densities more weight

--- src/Font.cpp
+++ src/Font.cpp
@@ -85,10 +85,10 @@ unsigned Font::Symbol::match(const Font::Symbol & other) const
 	//score += fabs(density.total - other.density.total) * 100;
 	score += fabs(proportion - other.proportion) * 100;
 	score += fabs(density.border - other.density.border) * 100;
-	score += fabs(density.q1 - other.density.q1) * 100;
-	score += fabs(density.q2 - other.density.q2) * 100;
-	score += fabs(density.q3 - other.density.q3) * 100;
-	score += fabs(density.q4 - other.density.q4) * 100;
+	score += fabs(density.q1 - other.density.q1) * 200;
+	score += fabs(density.q2 - other.density.q2) * 200;
+	score += fabs(density.q3 - other.density.q3) * 200;
+	score += fabs(density.q4 - other.density.q4) * 200;
 	//score += fabs(density.mid1 - other.density.mid1) * 100;
 	//score += fabs(density.mid2 - other.density.mid2) * 100;
 	return score;
